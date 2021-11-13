beb273abb516942c1db6a19771b4d9207214eef9
xiaoma555 <2637144005@qq.com>
2021-11-13 15:26:34 +0800

    游客页面后台完毕

--- message2.php
+++ message2.php
@@ -1,19 +1,41 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
-		include_once("conn.php");
-		$result = mysql_query("select * from message order by addTime desc");// 获取数据
+	$page=@$_GET['page'];
+		include("conn.php");
+		$result = mysql_query("select * from message where sh=1 order by addTime desc");// 获取数据
+		
+		
+		if(empty($page)){
+			$page=1;
+		}
+		if($page<1){
+			$page=1;
+		}
 		$num = mysql_num_rows($result);// 获取条数
+		$pagecount=ceil($num/5);//3
+		if($page>=$pagecount){
+			$page=$pagecount;
+		}
 		// 将条例按关联数组进行输出
 		$json = '{"status":"10001","msg":"OK","data":[';
-		if($num){
-			while($arr = mysql_fetch_assoc($result)){
+		mysql_data_seek($result,($page-1)*5);
+		for($i=1;$i<=5;$i++){
+			if($arr=mysql_fetch_assoc($result)){
 				$json.=json_encode($arr).',';
+				
 			}
-			echo substr($json,0,strlen($json)-1).']}';
+			
 		}
-		else{
-			'{"status":"30001","msg":"没有数据！"}';
-		}
+		echo substr($json,0,strlen($json)-1).']}';
+		// if($num){
+		// 	while($arr = mysql_fetch_assoc($result)){
+		// 		$json.=json_encode($arr).',';
+		// 	}
+		// 	echo substr($json,0,strlen($json)-1).']}';
+		// }
+		// else{
+		// 	'{"status":"30001","msg":"没有数据！"}';
+		// }
 		// 关闭结果集
 		mysql_free_result($result);
 		// 关闭数据库服务器
---
+++ rescount.php
@@ -0,0 +1,12 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+// if($_SERVER['REQUEST_METHOD']=="POST"){
+   include("conn.php");
+   $rs=mysql_query('select * from message where sh=1');
+   $rescount=mysql_num_rows($rs);
+   // echo '{"status":"10001","rescount":'.$rescount.'}';
+   echo $rescount;
+// }else{
+	// header("location:error.php");
+// }
+?>
